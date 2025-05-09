3e3b87d8e922b618a2e769fac27298063c783cd2
Clayton Forssen <clayton.forssen@swtq.se>
2025-05-09 15:01:12 +0200

    updated manifest and fixed icons

--- tabatarelease.service.worker.js
+++ tabatarelease.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1746795424|449237865';
+const CACHE_VERSION = '1746795621|645560992';
 /** @type {string} */
 const CACHE_PREFIX = 'SWTQ-workoutHTML-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
